7b689f5f92ea25c2ce1f2b9959de208fec30f4a8
Akash Patel <akash_patel97@hotmail.com>
2017-01-24 21:56:56 -0500

    return hello world

--- lolmessenger.py
+++ lolmessenger.py
@@ -2,14 +2,17 @@
 from flask import Flask, request
 import requests
 import json
-import rito
+from cassiopeia import riotapi
 
 
+#setup + auth tokens
 app = Flask(__name__)
-
-#Page Authentication Token for the page which the messenger will be accessed from
+#NA only 
+riotapi.set_region("NA")
+riotapi.set_api_key("YOUR-API-KEY-HERE")
 page_auth_token = ''
 verify_token = 'my_voice_is_my_password_verify_me'
+
 
 @app.route('/webhook', methods=['GET'])
 #Authenticate for Messenger
@@ -24,22 +27,47 @@ def auth():
 
 @app.route('/webhook', methods=['POST'])
 #Messages and how to respond to them
+def send_reply():
+	reply = "Hello World"
+	req = request.json()
+	sender_id = req["sender"]["id"]
+	headers = {
+		'Content-Type': 'application/json'
+	}
+	params = {
+		'access_token': page_auth_token
+	}
+	resp = {
+		'recipient': {
+			'id': sender_id
+		},
+		'message': {
+			'text': reply
+		}
+	}
+	r = requests.post('https://graph.facebook.com/v2.6/me/messages', params=params, json=resp, headers=headers)
+	return "Reply Sent"
 
+'''
+
+#helper functions
 def free_champs():
 	#will call riot api and return the free week champs
 
 
 def is_in_game(summoner_name):
 	#check if summoner_name is playing right now
-	
 
 
+#not a priority, get above two working first
 def champ_mastery(summoner_name, champ=None):
 	#top 3 champ masteries of summoner_name (Default)
 	#Champ mastery of 'champ' (second arg)
 	if champ == None:
 		#default
 	else:
+
+'''
 
 
 if __name__ == "__main__":
