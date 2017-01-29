cca01a762279267dde15f9c55eaef10ee10f3f57
Akash Patel <akash_patel97@hotmail.com>
2017-01-29 18:28:31 -0500

    key is str

--- lolmessenger.py
+++ lolmessenger.py
@@ -42,14 +42,14 @@ help_msg = """Supported messages:
 # ----- HELPER FUNCTIONS ------ #
 def get_free_champs():
 	free_champs_url = "https://na.api.pvp.net/api/lol/na/v1.2/champion?freeToPlay=true&api_key=" + riot_api_key
-	free_champs = requests.get(free_champs_url).json()
+	free_champs = requests.get(free_champs_url).json()["champions"]
 	#print free_champs
- 	lst_free_champs = [champ["id"] for champ in free_champs["champions"]]
+ 	lst_free_champs = [str(champ["id"]) for champ in free_champs["champions"]]
 	print lst_free_champs
 	all_champs_url = "https://global.api.pvp.net/api/lol/static-data/na/v1.2/champion?dataById=true&api_key=" + riot_api_key
-	all_champs = requests.get(all_champs_url).json()
+	all_champs = requests.get(all_champs_url).json()["data"]
 	#print all_champs
-	lst_names = [all_champs["data"][champ_id]["name"] for champ_id in lst_free_champs]
+	lst_names = [all_champs[champ_id]["name"] for champ_id in lst_free_champs]
 	print lst_names
 	#return free_champs
 
