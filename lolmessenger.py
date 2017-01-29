b4964a56ce70654c818ca67b4c4e74004ada20ab
Akash Patel <akash_patel97@hotmail.com>
2017-01-29 18:17:09 -0500

    wrong request

--- lolmessenger.py
+++ lolmessenger.py
@@ -47,7 +47,7 @@ def get_free_champs():
  	#lst_free_champs = [champ["id"] for champ in free_champs["champs"]]
 	#print lst_free_champs
 	all_champs_url = "https://global.api.pvp.net/api/lol/static-data/na/v1.2/champion?dataById=true&api_key=" + riot_api_key
-	all_champs = request.get(all_champs_url)
+	all_champs = requests.get(all_champs_url)
 	print all_champs
 	#lst_names = [all_champs["data"][champ_id]["name"] for champ_id in lst_free_champs]
 	#return free_champs
